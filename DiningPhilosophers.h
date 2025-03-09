bedc221a7a1128fef57a4ba4c331031cc62383be
justine <justia2004@gmail.com>
2025-03-09 15:59:50 +0100

    dining philosophers jeader file

---
+++ DiningPhilosophers.h
@@ -0,0 +1,23 @@
+#ifndef DININGPHILOSOPHERS_H
+#define DININGPHILOSOPHERS_H
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <random>
+
+class DiningPhilosophers {
+private:
+    int num_philosophers;  // Number of philosophers
+    std::vector<std::mutex> forks;  // Mutex locks representing forks
+    std::vector<std::thread> philosophers;  // Threads representing philosophers
+
+    void philosopher(int id);
+
+public:
+    DiningPhilosophers(int n);
+    void start();
+};
+
+#endif // DININGPHILOSOPHERS_H
